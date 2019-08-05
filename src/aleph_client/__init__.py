855f460c8ae923590f1820a2ef8b8a92d3a73e39
Henry Taieb <he.taieb@gmail.com>
2019-08-05 19:51:59 +0200

    commons

--- src/aleph_client/__init__.py
+++ src/aleph_client/__init__.py
@@ -9,3 +9,5 @@ except DistributionNotFound:
     __version__ = 'unknown'
 finally:
     del get_distribution, DistributionNotFound
+
+from .main import create_post, create_aggregate
---
+++ src/aleph_client/chains/common.py
@@ -0,0 +1,48 @@
+from secp256k1 import PrivateKey, PublicKey
+# In case we don't want to bother with handling private key ourselves
+# do an ugly and insecure write and read from disk to this file.
+PRIVATE_KEY_FILE = 'device.key'
+
+def get_verification_buffer(message):
+    """ Returns a serialized string to verify the message integrity
+    (this is was it signed)
+    """
+    return '{chain}\n{sender}\n{type}\n{item_hash}'.format(**message)\
+        .encode('utf-8')
+
+
+def get_public_key(private_key):
+    privkey = PrivateKey(private_key, raw=True)
+    return privkey.pubkey.serialize()
+
+class BaseAccount:
+    CHAIN = None
+    
+    def __init__(self):
+        raise NotImplementedError
+    
+    def sign_message(self, message):
+        raise NotImplementedError
+    
+    def get_address(self):
+        raise NotImplementedError
+    
+    def get_public_key(self):
+        raise NotImplementedError
+
+# Start of the ugly stuff
+def generate_key():
+    privkey = PrivateKey()
+    return privkey.private_key
+
+
+def get_fallback_private_key():
+    try:
+        with open(PRIVATE_KEY_FILE, "rb") as prvfile:
+            private_key = prvfile.read()
+    except OSError:
+        private_key = generate_key()
+        with open(PRIVATE_KEY_FILE, "wb") as prvfile:
+            prvfile.write(private_key)
+
+    return private_key
