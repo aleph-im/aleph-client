a10ace5eed3b993107c74c627d64c2cd7d4cd690
Henry Taieb <he.taieb@gmail.com>
2019-08-28 14:45:15 +0200

    make mqtt more generic, support tls

--- examples/mqtt.py
+++ examples/mqtt.py
@@ -10,12 +10,13 @@ import platform
 import time
 import aiomqtt
 import asyncio
+import click
 from aleph_client.main import create_aggregate, create_post
 
 # from aleph_client.chains.nuls1 import NULSAccount, get_fallback_account
-from aleph_client.chains.ethereum import ETHAccount, get_fallback_account
+from aleph_client.chains.ethereum import ETHAccount
+from aleph_client.chains.common import get_fallback_private_key
 
-ACCOUNT = None
 
 def get_input_data(value):
     if value == b'true':
@@ -28,9 +29,11 @@ def get_input_data(value):
     except ValueError:
         return value.decode('utf-8')
 
+
 def send_metrics(account, metrics):
     # metric_payload = {}
     return create_aggregate(account, 'metrics', metrics, channel='SYSINFO')
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -39,6 +42,7 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("/#")
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -52,17 +56,29 @@ def on_message(client, userdata, msg):
         curp = curp[part]
         
     curp[parts[-1]] = get_input_data(msg.payload)
-    print(state)
-
-async def main(loop):
-    loop = asyncio.get_event_loop()
-    account = get_fallback_account()
+    print(parts, msg.payload)
+    
+    
+async def gateway(loop, host='api1.aleph.im', port=1883, ca_cert=None,
+                  pkey=None, keepalive=60, transport='tcp', auth={}):
+    
+    if pkey is None:
+        pkey = get_fallback_private_key()
+        
+    account = ETHAccount(private_key=pkey)
     state = dict()
-    client = aiomqtt.Client(loop, userdata={'account': account, 'state': state})
+    client = aiomqtt.Client(loop,
+                            userdata={'account': account, 'state': state,
+                                      'auth': auth},
+                            transport=transport)
     client.on_connect = on_connect
     client.on_message = on_message
+    if ca_cert is not None:
+        client.tls_set(ca_cert)
+    if auth is not None:
+        client.username_pw_set(**auth)
 
-    await client.connect("localhost", 1883, 60)
+    await client.connect(host, port, keepalive)
     client.loop_start()
     # client.loop_forever()
     while True:
@@ -72,6 +88,29 @@ async def main(loop):
             print("sent", ret['item_hash'])
 
 
-if __name__ == '__main__':
+@click.command()
+@click.option('--host', default='localhost',
+              help='MQTT Broker host')
+@click.option('--port', default=1883, help='MQTT Broker port')
+@click.option('--user', default=None, help='MQTT Auth username')
+@click.option('--password', default=None, help='MQTT Auth password')
+@click.option('--use-tls', is_flag=True)
+@click.option('--ca-cert', default=None, help='CA Cert path')
+@click.option('--pkey', default=None, help='Account private key (optionnal, will default to device.key file)')
+def main(host, port, user, password, use_tls=False, ca_cert=None, pkey=None):
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(loop))
+    auth = None
+    if user is not None:
+        auth = {'username': user, 'password': password}
+        
+    if use_tls:
+        if ca_cert is None:
+            import certifi
+            ca_cert = certifi.where()
+            print(ca_cert)
+        
+    loop.run_until_complete(gateway(loop, host, port, ca_cert, pkey=pkey, auth=auth))
+
+
+if __name__ == '__main__':
+    main()
