7296c0e07a6876edd0e7b7eca6c3127195930e46
Hugo Herter <git@hugoherter.com>
2021-05-25 15:11:07 +0200

    Feature: Add CLI to post messages, upload and pin files

---
+++ src/aleph_client/__main__.py
@@ -0,0 +1,183 @@
+"""Aleph Client command-line interface.
+"""
+import asyncio
+import json
+import logging
+import os.path
+from enum import Enum
+from typing import Optional, Dict
+
+import typer
+from typer import echo
+
+from aleph_client.asynchronous import (
+    get_fallback_session,
+    sync_create_store,
+    sync_create_post,
+)
+from aleph_client.chains.common import get_fallback_private_key
+from aleph_client.chains.ethereum import ETHAccount
+
+logger = logging.getLogger(__name__)
+app = typer.Typer()
+
+
+class KindEnum(str, Enum):
+    json = "json"
+
+
+class StorageEnum(str, Enum):
+    ipfs = "ipfs"
+    storage = "storage"
+
+
+def _input_multiline() -> str:
+    """Prompt the user for a multiline input."""
+    print("Enter/Paste your content. Ctrl-D or Ctrl-Z ( windows ) to save it.")
+    contents = ""
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        contents += line + "\n"
+    return contents
+
+
+def _load_account(
+    private_key_str: Optional[str] = None, private_key_file: Optional[str] = None
+) -> ETHAccount:
+    """Load private key from a file"""
+
+    if private_key_str:
+        if not private_key_str:
+            return ETHAccount(private_key_str.encode())
+        else:
+            raise ValueError("Choose between a private key or a file, not both")
+    elif private_key_file:
+        with open(private_key_file, 'rb') as pk_fd:
+            private_key: bytes = pk_fd.read()
+        return ETHAccount(private_key)
+    else:
+        logger.info("Generating fallback private key")
+        private_key = get_fallback_private_key()
+        return ETHAccount(private_key=private_key)
+
+
+@app.command()
+def post(
+    path: Optional[str] = None,
+    type: str = "test",
+    channel: str = "TEST",
+    private_key: Optional[str] = None,
+    private_key_file: Optional[str] = None,
+):
+    """Post a message on Aleph.im."""
+
+    account = _load_account(private_key, private_key_file)
+    storage_engine: str
+    content: Dict
+
+    if path:
+        if not os.path.isfile(path):
+            print(f"Error: File not found: '{path}'")
+            raise typer.Exit(code=1)
+
+        file_size = os.path.getsize(path)
+        storage_engine = "ipfs" if file_size > 4 * 1024 * 1024 else "storage"
+
+        with open(path, "r") as fd:
+            content = json.load(fd)
+
+    else:
+        content_raw = _input_multiline()
+        storage_engine = "ipfs" if len(content_raw) > 4 * 1024 * 1024 else "storage"
+        try:
+            content = json.loads(content_raw)
+        except json.decoder.JSONDecodeError:
+            echo("Not valid JSON")
+            raise typer.Exit(code=2)
+
+    try:
+        result = sync_create_post(
+            account=account,
+            post_content=content,
+            post_type=type,
+            ref=None,
+            channel=channel,
+            inline=True,
+            storage_engine=storage_engine,
+        )
+        echo(f"{json.dumps(result, indent=4)}")
+    finally:
+        # Prevent aiohttp unclosed connector warning
+        asyncio.get_event_loop().run_until_complete(get_fallback_session().close())
+
+
+@app.command()
+def upload(
+    file_path: str,
+    channel: str = "TEST",
+    private_key: Optional[str] = None,
+    private_key_file: Optional[str] = None,
+):
+    """Upload and store a file on Aleph.im."""
+
+    account = _load_account(private_key, private_key_file)
+
+    try:
+        if not os.path.isfile(file_path):
+            print(f"Error: File not found: '{file_path}'")
+            raise typer.Exit(code=1)
+
+        with open(file_path, "rb") as fd:
+            logger.debug("Reading file")
+            # TODO: Read in lazy mode instead of copying everything in memory
+            file_content = fd.read()
+            storage_engine = (
+                "ipfs" if len(file_content) > 4 * 1024 * 1024 else "storage"
+            )
+            logger.debug("Uploading file")
+            result = sync_create_store(
+                account=account,
+                file_content=file_content,
+                storage_engine=storage_engine,
+                channel=channel,
+            )
+            logger.debug("Upload finished")
+            echo(f"{json.dumps(result, indent=4)}")
+    finally:
+        # Prevent aiohttp unclosed connector warning
+        asyncio.get_event_loop().run_until_complete(get_fallback_session().close())
+
+
+@app.command()
+def pin(
+    hash: str,
+    channel: str = "TEST",
+    private_key: Optional[str] = None,
+    private_key_file: Optional[str] = None,
+):
+    """Persist a file from IPFS on Aleph.im."""
+
+    account = _load_account(private_key, private_key_file)
+
+    try:
+        result = sync_create_store(
+            account=account,
+            file_hash=hash,
+            storage_engine="ipfs",
+            channel=channel,
+        )
+        logger.debug("Upload finished")
+        echo(f"{json.dumps(result, indent=4)}")
+    finally:
+        # Prevent aiohttp unclosed connector warning
+        asyncio.get_event_loop().run_until_complete(get_fallback_session().close())
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+    )
+    app()
