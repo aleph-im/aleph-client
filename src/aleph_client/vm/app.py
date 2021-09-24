d345ab5d7a02e22ad400085396dc5e5ad239eb2d
Hugo Herter <git@hugoherter.com>
2021-09-24 17:45:15 +0200

    Feature: Provide Aleph VM apps an API to handle events

---
+++ src/aleph_client/vm/app.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+from typing import List, Dict, Callable, Awaitable, Optional
+
+AsgiApplication = Callable
+
+
+@dataclass
+class EventHandler:
+    filters: List[Dict]
+    handler: Callable
+
+    def matches(self, scope: Dict) -> bool:
+        for filter in self.filters:
+            # if [filter matches scope]: TODO
+            if True:
+                return True
+        return False
+
+
+class AlephApp:
+    http_app: Optional[AsgiApplication] = None
+    event_handlers: List[EventHandler]
+
+    def __init__(self, http_app: Optional[AsgiApplication] = None):
+        self.http_app = http_app
+        self.event_handlers = []
+
+    def event(self, filters: List[Dict]):
+        """Use this decorator to register event calls.
+
+        ```python
+            @app.event(filters=[...])
+            def on_event(event):
+                ...
+        ```
+        """
+        def inner(func: Callable):
+            # Register the event handler
+            event_handler = EventHandler(filters=filters,
+                                         handler=func)
+            self.event_handlers.append(event_handler)
+            return func
+        return inner
+
+    def __call__(self, scope: Dict, receive: Awaitable, send: Callable[[Dict], Awaitable]):
+        if scope['type'] in ("http", "websocket"):
+            if self.http_app:
+                return self.http_app(scope=scope, receive=receive, send=send)
+            else:
+                raise ValueError("No HTTP app registered")
+        elif scope['type'] == "aleph.message":
+            for event_handler in self.event_handlers:
+                if event_handler.matches(scope):
+                    # event_handler.handler(scope=scope, receive=receive, send=send)
+                    async def send_handler_result():
+                        result = await event_handler.handler(event=scope)
+                        await send(result)
+                    return send_handler_result()
+        else:
+            raise ValueError(f"Unknown scope type '{scope['type']}'")
+
+
+    def __getattr__(self, name):
+        # Default all calls to the HTTP handler
+        return getattr(self.http_app, name)
---
+++ tests/test_vm_app.py
@@ -0,0 +1,30 @@
+import asyncio
+
+from aleph_client.vm.app import AlephApp
+
+
+def test_app():
+
+    # Create a test app
+    app = AlephApp()
+
+    @app.event(filters=[])
+    async def aleph_event(event):
+        print("aleph_event", event)
+
+    # Call the app with an ASGI context
+    scope = {
+        'type': "aleph.message",
+    }
+
+    async def receive():
+        return {'type': 'aleph.message',
+                'body': b"BODY",
+                'more_body': False}
+
+    send_queue: asyncio.Queue = asyncio.Queue()
+
+    async def send(dico):
+        await send_queue.put(dico)
+
+    app(scope, receive, send)
