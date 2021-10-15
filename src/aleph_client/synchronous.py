57940d6900c5d6a1d3b4894e42e72b956d3201f7
Hugo Herter <git@hugoherter.com>
2021-10-15 11:47:54 +0200

    Cleanup: Document and improve wrap_async()

--- src/aleph_client/synchronous.py
+++ src/aleph_client/synchronous.py
@@ -12,10 +12,18 @@ from .types import Account, StorageEnum
 
 
 def wrap_async(func):
+    """Wrap an asynchronous function into a synchronous one,
+    for easy use in synchronous code.
+    """
     def func_caller(*args, **kwargs):
         loop = asyncio.get_event_loop()
         return loop.run_until_complete(func(*args, **kwargs))
 
+    # Copy wrapped function interface:
+    func_caller.__doc__ = func.__doc__
+    func_caller.__annotations__ = func.__annotations__
+    func_caller.__defaults__ = func.__defaults__
+    func_caller.__kwdefaults__ = func.__kwdefaults__
     return func_caller
 
 
