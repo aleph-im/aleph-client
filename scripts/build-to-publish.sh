a26d0114215582b73bd7fd7da0372e4b3d932e47
Hugo Herter <git@hugoherter.com>
2022-03-09 09:53:46 +0100

    Fix: Script build-to-publish.sh required `dist` directory

--- scripts/build-to-publish.sh
+++ scripts/build-to-publish.sh
@@ -10,5 +10,11 @@ else
   DOCKER_COMMAND=docker
 fi
 
+mkdir -p ./dist
+chmod 0777 ./dist
+
 $DOCKER_COMMAND build -t aleph-client -f docker/Dockerfile .
-$DOCKER_COMMAND run -ti --rm -v "$(pwd)/dist":/opt/aleph-client/dist aleph-client bash
+$DOCKER_COMMAND run -ti --rm \
+  -w /opt/aleph-client \
+  -v "$(pwd)/dist":/opt/aleph-client/dist \
+  aleph-client bash
