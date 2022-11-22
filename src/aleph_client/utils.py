cb1c3421fe8af3768de59bc8f0857936a0e628f0
aliel <ali.elb@gmail.com>
2022-11-22 19:47:14 +0100

    WIP

--- src/aleph_client/utils.py
+++ src/aleph_client/utils.py
@@ -2,8 +2,9 @@ import logging
 import os
 from pathlib import Path
 from shutil import make_archive
-from typing import Tuple, Type
+from typing import Tuple, Type, Optional, List
 from zipfile import ZipFile, BadZipFile
+from unittest.mock import patch
 
 from aleph_message.models import MessageType
 from aleph_message.models.program import Encoding
@@ -29,19 +30,24 @@ def try_open_zip(path: Path) -> None:
             if not archive.namelist():
                 raise BadZipFile("No file in the archive.")
 
-
-def create_archive(path: Path) -> Tuple[Path, Encoding]:
+def create_archive(path: Path, exclude: Optional[List[Path]] = None) -> Tuple[Path, Encoding]:
     """Create a zip archive from a directory"""
     if os.path.isdir(path):
         if settings.CODE_USES_SQUASHFS:
             logger.debug("Creating squashfs archive...")
             archive_path = Path(f"{path}.squashfs")
-            os.system(f"mksquashfs {path} {archive_path} -noappend")
+            if exclude is None:
+                os.system(f"mksquashfs {path} {archive_path} -noappend")
+            else:
+                to_exclude = ' '.join(exclude)
+                os.system(f"mksquashfs {path} {archive_path} -noappend -wildcards -e {to_exclude}")
             assert archive_path.is_file()
             return archive_path, Encoding.squashfs
         else:
             logger.debug("Creating zip archive...")
-            make_archive(str(path), "zip", path)
+            os_path_isfile = os.path.isfile
+            with patch("os.path.isfile", side_effect=lambda _path: False if(_path in exclude) else os_path_isfile(_path)):
+                make_archive(str(path), "zip", path)
             archive_path = Path(f"{path}.zip")
             return archive_path, Encoding.zip
     elif os.path.isfile(path):
