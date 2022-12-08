28dfb483a4c9f013807603e436010304d8a0a1e3
aliel <ali.elb@gmail.com>
2022-12-08 23:05:50 +0100

    Add ALEPH_IM_HOME env Store private key file in ALEPH_IM_HOME dir #119

--- src/aleph_client/conf.py
+++ src/aleph_client/conf.py
@@ -3,15 +3,20 @@ from shutil import which
 from typing import Optional
 
 from pydantic import BaseSettings, Field
+import os
 
 
 class Settings(BaseSettings):
+    ALEPH_IM_HOME: Optional[str] = None
+
     # In case the user does not want to bother with handling private keys himself,
     # do an ugly and insecure write and read from disk to this file.
-    PRIVATE_KEY_FILE: Path = Field(
-        default=Path("device.key"),
-        description="Path to the private key used to sign messages",
-    )
+    @property
+    def PRIVATE_KEY_FILE(self) -> Path:
+        """
+        Path to the private key used to sign messages
+        """
+        return os.path.join(self.ALEPH_IM_HOME, "private-keys", "device.key")
 
     PRIVATE_KEY_STRING: Optional[str] = None
     API_HOST: str = "https://api2.aleph.im"
@@ -38,6 +43,18 @@ class Settings(BaseSettings):
         case_sensitive = False
         env_file = ".env"
 
-
 # Settings singleton
 settings = Settings()
+
+if settings.ALEPH_IM_HOME is None:
+    if os.environ.get("ALEPH_IM_HOME") is not None:
+        os.environ["ALEPH_ALEPH_IM_HOME"] = os.environ.get("ALEPH_IM_HOME")
+    else:
+        xdg_data_home = os.environ.get("XDG_DATA_HOME")
+        if xdg_data_home is not None:
+            os.environ["ALEPH_ALEPH_IM_HOME"] = os.path.join(xdg_data_home, ".aleph-im")
+        else:
+            home = os.path.expanduser("~")
+            os.environ["ALEPH_ALEPH_IM_HOME"] = os.path.join(home, ".aleph-im")
+
+    settings = Settings()
