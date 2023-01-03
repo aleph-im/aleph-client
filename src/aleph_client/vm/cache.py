cee55e2dda6d7c4994061bc4eded6782f9247a73
Mike Hukiewitz <mike.hukiewitz@robotter.ai>
2023-01-03 14:08:32 +0100

    Problem: Programs that work on Aleph VMs and use VmCache cannot work locally
    Solution: Introduce abstract base class for VmCache and implement LocalVmCache to mock VmCache functionality

--- src/aleph_client/vm/cache.py
+++ src/aleph_client/vm/cache.py
@@ -1,4 +1,5 @@
 import re
+import abc
 from typing import Union, Optional
 
 from aiohttp import ClientSession
@@ -7,15 +8,33 @@ from aleph_client.asynchronous import get_fallback_session
 from ..conf import settings
 
 
-class VmCache:
-    """
-    Virtual Machines can use this cache to store temporary data in memory on the host.
-    """
+class BaseVmCache(abc.ABC):
+    """Virtual Machines can use this cache to store temporary data in memory on the host."""
 
     session: ClientSession
 
     def __init__(self, session: Optional[ClientSession] = None):
         self.session = session or get_fallback_session()
+
+    @abc.abstractmethod
+    async def get(self, key: str):
+        pass
+
+    @abc.abstractmethod
+    async def set(self, key: str, value: Union[str, bytes]):
+        pass
+
+    @abc.abstractmethod
+    async def delete(self, key: str):
+        pass
+
+    @abc.abstractmethod
+    async def keys(self, pattern: str = "*"):
+        pass
+
+
+class VmCache(BaseVmCache):
+    """Virtual Machines can use this cache to store temporary data in memory on the host."""
 
     async def get(self, key: str):
         if not re.match(r"^\w+$", key):
@@ -47,10 +66,36 @@ class VmCache:
     async def keys(self, pattern: str = "*"):
         if not re.match(r"^[\w?*^\-]+$", pattern):
             raise ValueError(
-                "Patterh may only contain letters, numbers, underscore, ?, *, ^, -"
+                "Pattern may only contain letters, numbers, underscore, ?, *, ^, -"
             )
         async with self.session.get(
             f"{settings.API_HOST}/cache/?pattern={pattern}"
         ) as resp:
             resp.raise_for_status()
             return await resp.json()
+
+
+class LocalVmCache:
+    """This is a local cache that can be used for testing purposes."""
+
+    def __init__(self):
+        self._cache = {}
+
+    async def get(self, key: str):
+        return self._cache.get(key)
+
+    async def set(self, key: str, value: Union[str, bytes]):
+        self._cache[key] = value
+
+    async def delete(self, key: str):
+        del self._cache[key]
+
+    async def keys(self, pattern: str = "*"):
+        if not re.match(r"^[\w?*^\-]+$", pattern):
+            raise ValueError(
+                "Pattern may only contain letters, numbers, underscore, ?, *, ^, -"
+            )
+        all_keys = list(self._cache.keys())
+        if pattern == "*":
+            return all_keys
+        return [key for key in all_keys if re.match(pattern, key)]
