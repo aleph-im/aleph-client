2595ab087e852af26d856b461a1187e270a55a3c
AmozPay <amoz.pay@epitech.eu>
2023-01-06 14:48:12 +0100

    Problem: runtime creation and testing procedures could be improved Solution: Add 'init' and 'test' commands to init a runtime project and test it

--- src/aleph_client/commands/runtime/runtime.py
+++ src/aleph_client/commands/runtime/runtime.py
@@ -3,11 +3,14 @@ from pathlib import Path
 from enum import Enum
 import logging
 from pathlib import Path
-from .firecracker import download_firecracker
-from .templates import download_templates
+from .init_command.download_firecracker import download_firecracker
+from .init_command.download_templates import download_templates
 import os
 from sys import stderr
-
+import subprocess
+import sys
+import time
+import signal
 import typer
 
 logger = logging.getLogger(__name__)
@@ -18,10 +21,7 @@ class BuilderOption(str, Enum):
     docker = "docker"
     podman = "podman"
 
-
-
 app = typer.Typer()
-
 
 @app.command()
 def init(
@@ -40,25 +40,66 @@ def init(
     download_firecracker(dest)
     download_templates(dest, sdk_version)
 
+def build_runtime_image(
+    project: Path,
+    builder: BuilderOption,
+    tag: str
+) -> str:
+    project_path = os.path.abspath(project)
+    project_name = os.path.basename(project_path)
+    image_name = f"{project_name}:{tag}"
+    context = os.path.join(project_path, "plugins")
+    containerfile = os.path.join(project_path, "plugins/Containerfile")
+    os.system(f"{builder} build -f {containerfile} -t {image_name} {context}")
+    return image_name
+
+def export_container(builder: BuilderOption, container_name: str, dest: Path):
+    os.makedirs(dest, exist_ok=True)
+    tar_path = os.path.join(dest, "rootfs.tar")
+    rootfs_dir = os.path.join(dest, "rootfs")
+    os.system(f"{builder} export {container_name} > {tar_path}")
+    os.makedirs(rootfs_dir, exist_ok=True)
+    os.system(f"tar -xf {tar_path} --directory={rootfs_dir}")
+    squashfs = os.path.join(dest, "rootfs.squashfs")
+    if os.path.exists(squashfs):
+        os.remove(squashfs)
+    os.system(f"mksquashfs {rootfs_dir} {squashfs}")
+
 @app.command()
 def build(
-    project: Path = typer.Argument(..., help="The name of your project"),
-    test: bool = typer.Option(False, help="Build a test runtime to test your plugins locally"),
+    project: Path = typer.Argument(".", help="The name of your project"),
     builder: BuilderOption = typer.Option(BuilderOption.podman, help="Choose between docker and podman to build your runtime"),
-    version: str = typer.Option("latest", help="tag of your runtime version")
+    tag: str = typer.Option("latest", help="tag of your runtime tag")
 ):
     """Build a runtime created with the init command, using podman or docker"""
-    image_name = f"{project}:{version}"
-    context = f"{project}/plugins"
-    containerfile = f"{project}/plugins/Containerfile"
-    os.system(f"{builder} build -f {containerfile} -t {image_name} {context}")
-    os.system(f"{builder} container create --name {project} {image_name}")
-    os.makedirs(f"{project}/build", exist_ok=True)
-    os.system(f"{builder} export {project} > {project}/build/rootfs.tar")
-    os.makedirs(f"{project}/build/rootfs", exist_ok=True)
-    os.system(f"tar -xf {project}/build/rootfs.tar --directory={project}/build/rootfs")
-    os.system(f"{builder} container rm {project}")
-    os.system(f"mksquashfs {project}/build/rootfs {project}/build/rootfs.squashfs")
+    project_path = os.path.abspath(project)
+    image_name = build_runtime_image(project, builder, tag)
+    os.makedirs(os.path.join(project_path, "build"), exist_ok=True)
+    container_name = image_name.split(":")[0] + "_export"
+    os.system(f"{builder} container create --name {container_name} {image_name}")
+    export_container(builder, container_name, os.path.join(project_path, "build/release"))
+    os.system(f"{builder} container rm {container_name}")
 
+@app.command()
+def test(
+    project: Path = typer.Argument(".", help="The name of your project"),
+    is_build: bool = typer.Option(False, "--build", help="If --build, build the runtime before testing. Else, finds runtime in project/build/"),
+    builder: BuilderOption = typer.Option(BuilderOption.podman, help="Same as aleph runtime build --builder"),
+    tag: str = typer.Option("latest", help="Same as aleph runtime build --tag")
+):
+    project_path = os.path.abspath(project)
+    project_name = os.path.basename(project_path)
+    image_name = f"{project_name}:{tag}"
+    if is_build:
+        build_runtime_image(project, builder, tag)
+    container_name = project_name + "_test"
+    os.system(f"{builder} run -it --name {container_name} {image_name} touch /root/sdk_test")
+    os.system(f"{builder} cp plugins/test_data.py {container_name}:/root/test_data.py")
+    os.system(f"{builder} commit {container_name} {project_name}:test")
+    export_container(builder, container_name, os.path.join(project_path, "build/test"))
 
-
+    firecracker_socket = "/tmp/firecracker.socket"
+    firecracker_config_file = "vm_config.json"
+    if os.path.exists(firecracker_socket):
+        os.system(f"sudo rm {firecracker_socket}")
+    os.system(f"sudo ./firecracker/firecracker --api-sock {firecracker_socket} --config-file {firecracker_config_file}")
