70c076d9dfa572b6c036dba83f6c2948255b84f5
Mike Hukiewitz <70762838+MHHukiewitz@users.noreply.github.com>
2023-01-11 16:32:07 +0000

    Feat: Add LocalVmCache for testing purposes (#133)
    Problem: Programs that work on Aleph VMs and use VmCache cannot work locally
    
    Solution: Introduce abstract base class for VmCache and implement LocalVmCache to mock VmCache functionality

--- src/aleph_client/vm/cache.py
+++ src/aleph_client/vm/cache.py
@@ -1,5 +1,7 @@
 import re
-from typing import Union, Optional
+import fnmatch
+import abc
+from typing import Union, Optional, Any, Dict, List, NewType
 
 from aiohttp import ClientSession
 
@@ -7,50 +9,115 @@ from aleph_client.asynchronous import get_fallback_session
 from ..conf import settings
 
 
-class VmCache:
-    """
-    Virtual Machines can use this cache to store temporary data in memory on the host.
-    """
+CacheKey = NewType("CacheKey", str)
+
+
+def sanitize_cache_key(key: str) -> CacheKey:
+    if not re.match(r"^\w+$", key):
+        raise ValueError("Key may only contain letters, numbers and underscore")
+    return CacheKey(key)
+
+
+class BaseVmCache(abc.ABC):
+    """Virtual Machines can use this cache to store temporary data in memory on the host."""
 
     session: ClientSession
 
     def __init__(self, session: Optional[ClientSession] = None):
         self.session = session or get_fallback_session()
 
-    async def get(self, key: str):
-        if not re.match(r"^\w+$", key):
-            raise ValueError("Key may only contain letters, numbers and underscore")
-        async with self.session.get(f"{settings.API_HOST}/cache/{key}") as resp:
+    @abc.abstractmethod
+    async def get(self, key: str) -> Optional[bytes]:
+        """Get the value for a given key string."""
+        pass
+
+    @abc.abstractmethod
+    async def set(self, key: str, value: Union[str, bytes]) -> Any:
+        """Set the value for a given key string."""
+        pass
+
+    @abc.abstractmethod
+    async def delete(self, key: str) -> Any:
+        """Delete the value for a given key string."""
+        pass
+
+    @abc.abstractmethod
+    async def keys(self, pattern: str = "*") -> List[str]:
+        """Get all keys matching a given glob pattern."""
+        pass
+
+
+class VmCache(BaseVmCache):
+    """Virtual Machines can use this cache to store temporary data in memory on the host."""
+
+    cache: Dict[str, bytes]
+    api_host: str
+
+    def __init__(self, session: Optional[ClientSession] = None, api_host: Optional[str] = None):
+        super().__init__(session)
+        self.cache = {}
+        self.api_host = api_host if api_host else settings.API_HOST
+
+    async def get(self, key: str) -> Optional[bytes]:
+        sanitized_key = sanitize_cache_key(key)
+        async with self.session.get(f"{self.api_host}/cache/{sanitized_key}") as resp:
             if resp.status == 404:
                 return None
 
             resp.raise_for_status()
             return await resp.read()
 
-    async def set(self, key: str, value: Union[str, bytes]):
-        if not re.match(r"^\w+$", key):
-            raise ValueError("Key may only contain letters, numbers and underscore")
+    async def set(self, key: str, value: Union[str, bytes]) -> Any:
+        sanitized_key = sanitize_cache_key(key)
         data = value if isinstance(value, bytes) else value.encode()
         async with self.session.put(
-            f"{settings.API_HOST}/cache/{key}", data=data
+            f"{self.api_host}/cache/{sanitized_key}", data=data
         ) as resp:
             resp.raise_for_status()
             return await resp.json()
 
-    async def delete(self, key: str):
-        if not re.match(r"^\w+$", key):
-            raise ValueError("Key may only contain letters, numbers and underscore")
-        async with self.session.delete(f"{settings.API_HOST}/cache/{key}") as resp:
+    async def delete(self, key: str) -> Any:
+        sanitized_key = sanitize_cache_key(key)
+        async with self.session.delete(f"{self.api_host}/cache/{sanitized_key}") as resp:
             resp.raise_for_status()
             return await resp.json()
 
-    async def keys(self, pattern: str = "*"):
+    async def keys(self, pattern: str = "*") -> List[str]:
         if not re.match(r"^[\w?*^\-]+$", pattern):
             raise ValueError(
-                "Patterh may only contain letters, numbers, underscore, ?, *, ^, -"
+                "Pattern may only contain letters, numbers, underscore, ?, *, ^, -"
             )
         async with self.session.get(
-            f"{settings.API_HOST}/cache/?pattern={pattern}"
+            f"{self.api_host}/cache/?pattern={pattern}"
         ) as resp:
             resp.raise_for_status()
             return await resp.json()
+
+
+class TestVmCache(BaseVmCache):
+    """This is a local, dict-based cache that can be used for testing purposes."""
+
+    def __init__(self, session: Optional[ClientSession] = None):
+        super().__init__(session)
+        self._cache: Dict[str, bytes] = {}
+
+    async def get(self, key: str) -> Optional[bytes]:
+        sanitized_key = sanitize_cache_key(key)
+        return self._cache.get(sanitized_key)
+
+    async def set(self, key: str, value: Union[str, bytes]) -> None:
+        sanitized_key = sanitize_cache_key(key)
+        data = value if isinstance(value, bytes) else value.encode()
+        self._cache[sanitized_key] = data
+
+    async def delete(self, key: str) -> None:
+        sanitized_key = sanitize_cache_key(key)
+        del self._cache[sanitized_key]
+
+    async def keys(self, pattern: str = "*") -> List[str]:
+        if not re.match(r"^[\w?*^\-]+$", pattern):
+            raise ValueError(
+                "Pattern may only contain letters, numbers, underscore, ?, *, ^, -"
+            )
+        all_keys = list(self._cache.keys())
+        return fnmatch.filter(all_keys, pattern)
---
+++ tests/unit/test_vm_cache.py
@@ -0,0 +1,31 @@
+import pytest
+
+from aleph_client.vm.cache import TestVmCache, sanitize_cache_key
+
+
+@pytest.mark.asyncio
+async def test_local_vm_cache():
+    cache = TestVmCache()
+    assert (await cache.get("doesnotexist")) is None
+    assert len(await (cache.keys())) == 0
+    key = "thisdoesexist"
+    value = "yay, I exist!"
+    await cache.set(key, value)
+    assert (await cache.get(key)).decode() == value
+    assert (await cache.keys())[0] == key
+    assert (await cache.keys("*exist"))[0] == key
+    await cache.delete(key)
+    assert (await cache.get(key)) is None
+    assert len(await (cache.keys())) == 0
+
+
+def test_sanitize_cache_keys():
+    assert sanitize_cache_key("abc")
+    assert sanitize_cache_key("abc123")
+    assert sanitize_cache_key("abc_123")
+    with pytest.raises(ValueError):
+        sanitize_cache_key("abc-123")
+    with pytest.raises(ValueError):
+        sanitize_cache_key("abc!123")
+    with pytest.raises(ValueError):
+        assert sanitize_cache_key("*")
