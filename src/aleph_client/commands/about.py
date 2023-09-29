b4af5b96a5c6040d38d445a247659f4ad10af5ea
aliel <ali.elb@gmail.com>
2023-09-29 12:03:58 +0200

    Feature: Expose Aleph CLI Version #168

---
+++ src/aleph_client/commands/about.py
@@ -0,0 +1,20 @@
+import typer
+from pkg_resources import get_distribution
+
+app = typer.Typer()
+
+
+def get_version(value: bool):
+    __version__ = "NaN"
+    dist_name = "aleph-client"
+    if value:
+        try:
+            __version__ = get_distribution(dist_name).version
+        finally:
+            typer.echo(f"Aleph CLI Version: {__version__}")
+            raise typer.Exit(1)
+
+
+@app.command()
+def version():
+    get_version(True)
