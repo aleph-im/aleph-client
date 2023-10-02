c426aa67514fb84032f74f8eb074ffb70a080b68
1yam <lyam.gomes@epitech.eu>
2023-10-02 12:54:50 +0200

    Fix: Un used test

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -7,8 +7,7 @@ from aleph_message.models import (
     StoreMessage,
 )
 
-from aleph_client.commands.utils import write_file_from_bytes
-from aleph_client.utils import get_message_type_value
+from src.aleph_client.utils import get_message_type_value
 
 
 def test_get_message_type_value():
@@ -17,16 +16,3 @@ def test_get_message_type_value():
     assert get_message_type_value(StoreMessage) == MessageType.store
     assert get_message_type_value(ProgramMessage) == MessageType.program
     assert get_message_type_value(ForgetMessage) == MessageType.forget
-
-
-def test_write_file_from_bytes():
-    path = "./file.txt"
-    content = b"This is a Test file content."
-
-    # Call the function
-    write_file_from_bytes(path, content)
-
-    # Check if the file was written correctly
-    with open(path, "rb") as file:
-        file_content = file.read()
-    assert file_content == b"This is a Test file content."
