74fba9cadaa153cc0b4995cb4757b38e36018269
Alie.E <ali.elb@gmail.com>
2023-11-09 11:35:58 +0100

    Feature: Expose Aleph CLI Version #168 (#169)

--- src/aleph_client/__main__.py
+++ src/aleph_client/__main__.py
@@ -4,15 +4,22 @@ Aleph Client command-line interface.
 
 import typer
 
-from .commands import account, aggregate, files, message, program
+from .commands import about, account, aggregate, files, message, program
 
 app = typer.Typer()
+
+@app.callback()
+def common(
+    ctx: typer.Context,
+    version: bool = typer.Option(None, "--version", callback=about.get_version, help="Show Aleph CLI Version"),
+    v: bool = typer.Option(None, "-v", callback=about.get_version, help="Show Aleph CLI Version"),
+):
+    pass
 
 app.add_typer(account.app, name="account", help="Manage account")
 app.add_typer(
     aggregate.app, name="aggregate", help="Manage aggregate messages on aleph.im"
 )
-
 app.add_typer(
     files.app, name="file", help="File uploading and pinning on IPFS and aleph.im"
 )
@@ -23,6 +30,9 @@ app.add_typer(
 )
 app.add_typer(
     program.app, name="program", help="Upload and update programs on aleph.im VM"
+)
+app.add_typer(
+    about.app, name="about", help="Display the informations of Aleph CLI"
 )
 
 
