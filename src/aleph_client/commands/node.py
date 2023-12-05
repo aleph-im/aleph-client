310228f91768b103aa8cdf24bc95e9a56d7094b9
1yam <40899431+1yam@users.noreply.github.com>
2023-12-05 16:53:55 +0100

    Update src/aleph_client/commands/node.py
    Co-authored-by: Mike Hukiewitz <70762838+MHHukiewitz@users.noreply.github.com>

--- src/aleph_client/commands/node.py
+++ src/aleph_client/commands/node.py
@@ -142,7 +142,7 @@ async def compute(
     json: bool = typer.Option(
         default=False, help="Print as json instead of rich table"
     ),
-    active: bool = typer.Option(default=False, help="Only show active node"),
+    active: bool = typer.Option(default=False, help="Only show active nodes"),
     address: Optional[str] = typer.Option(default=None, help="adress owner"),
     debug: bool = False,
 ):
