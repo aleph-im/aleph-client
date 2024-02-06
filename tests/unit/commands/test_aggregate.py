a5b31389fe99117422b064fbf6f3fb126669398c
Darley Araújo Silva <darley.silva@funttastic.com>
2024-02-06 19:22:23 -0300

    Improving unittests at test_aggregate.py.

--- tests/unit/commands/test_aggregate.py
+++ tests/unit/commands/test_aggregate.py
@@ -10,11 +10,21 @@ runner = CliRunner()
 
 @pytest.mark.skip(reason="Not implemented.")
 def test_aggregate_forget(account_file: Path):
-    key = "key"
-    channel = "channel"
+    key = None
+    channel = None
+    private_key = None
+    private_key_file = str(account_file)
+    reason = None
+    debug = "--no-debug"
 
     result = runner.invoke(
-        app, ["aggregate", "forget", key, "--channel", channel, "--private-key-file", str(account_file)]
+        app, [
+            "aggregate", "forget", key,
+            "--channel", channel,
+            "--reason", reason,
+            "--private-key-file", private_key_file,
+            debug
+        ]
     )
 
     assert result.exit_code == 0, result.stdout
@@ -22,11 +32,19 @@ def test_aggregate_forget(account_file: Path):
 
 @pytest.mark.skip(reason="Not implemented.")
 def test_aggregate_get(account_file: Path):
-    key = "key"
-    address = "address"
+    key = None
+    address = None
+    private_key = None
+    private_key_file = str(account_file)
+    debug = "--no-debug"
 
     result = runner.invoke(
-        app, ["aggregate", "get", key, "--address", address, "--private-key-file", str(account_file)]
+        app, [
+            "aggregate", "get", key,
+            "--address", address,
+            "--private-key-file", private_key_file,
+            debug
+        ]
     )
 
     assert result.exit_code == 0, result.stdout
@@ -34,15 +52,26 @@ def test_aggregate_get(account_file: Path):
 
 @pytest.mark.skip(reason="Not implemented.")
 def test_aggregate_post(account_file: Path):
-    key = "key"
-    content = "{'c': 3, 'd': 4}"
-    address = "address"
+    key = None
+    content = None
+    address = None
+    private_key = None
+    private_key_file = str(account_file)
     channel = "channel"
-    inline = "no-inline"
-    sync = "no-sync"
+    inline = "--no-inline"
+    sync = "--no-sync"
+    debug = "--no-debug"
 
     result = runner.invoke(
-        app, ["aggregate", "post", key, content, "--address", address, "--channel", channel, "--inline", inline, "--sync", sync, "--private-key-file", str(account_file)]
+        app, [
+            "aggregate", "post", key, content,
+            "--address", address,
+            "--channel", channel,
+            "--private-key-file", private_key_file,
+            inline,
+            sync,
+            debug
+        ]
     )
 
     assert result.exit_code == 0, result.stdout
