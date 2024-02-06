8e95d7cb6ab4600c1d104aa60f71ce56dabf4ea3
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-02-06 22:26:22 +0000

    Improving unit test files.

--- tests/unit/commands/test_account.py
+++ tests/unit/commands/test_account.py
@@ -13,7 +13,11 @@ def test_account_address(account_file: Path):
     private_key_file = str(account_file)
 
     result = runner.invoke(
-        app, ["account", "address", "--private-key-file", private_key_file]
+        app, [
+            "account",
+            "address",
+            "--private-key-file", private_key_file
+        ]
     )
 
     assert result.exit_code == 0
@@ -25,20 +29,38 @@ def test_account_address(account_file: Path):
 
 @pytest.mark.skip(reason="Not implemented. It's failing the retrieve the balance for the address.")
 def test_account_balance(account_file: Path):
+    address = None
     private_key = None
     private_key_file = str(account_file)
 
     result = runner.invoke(
-        app, ["account", "balance", "--private-key-file", private_key_file]
+        app, [
+            "account",
+            "balance",
+            "--address", address,
+            "--private-key-file", private_key_file
+        ]
     )
 
     assert result.exit_code == 0
 
 
 def test_account_create(account_file: Path):
+    private_key = None
+    private_key_file = str(account_file)
+    replace = "--replace"
+    debug = "--no-debug"
+
     old_key = account_file.read_bytes()
+
     result = runner.invoke(
-        app, ["account", "create", "--replace", "--private-key-file", str(account_file)]
+        app, [
+            "account",
+            "create",
+            "--private-key-file", private_key_file,
+            replace,
+            debug
+        ]
     )
 
     assert result.exit_code == 0, result.stdout
@@ -49,8 +71,15 @@ def test_account_create(account_file: Path):
 
 
 def test_account_export_private_key(account_file: Path):
+    private_key = None
+    private_key_file = str(account_file)
+
     result = runner.invoke(
-        app, ["account", "export-private-key", "--private-key-file", str(account_file)]
+        app, [
+            "account",
+            "export-private-key",
+            "--private-key-file", private_key_file
+        ]
     )
     assert result.exit_code == 0
 
@@ -60,19 +89,28 @@ def test_account_export_private_key(account_file: Path):
 
 
 def test_account_path():
-    result = runner.invoke(app, ["account", "path"])
+    result = runner.invoke(app, [
+        "account",
+        "path"
+    ])
 
     assert result.stdout.startswith("/")
 
 
-def test_sign_raw():
+def test_sign_bytes_raw(account_file: Path):
+    message = "some message"
+    private_key = ""
+    private_key_file = str(account_file)
+    debug = "--no-debug"
+
     result = runner.invoke(
         app,
         [
             "account",
             "sign-bytes",
-            "--message",
-            "some message",
+            "--message", message,
+            "--private-key-file", private_key_file,
+            debug
         ],
     )
 
@@ -81,13 +119,19 @@ def test_sign_raw():
     assert "0x" in result.stdout
 
 
-def test_sign_raw_stdin():
+def test_sign_bytes_raw_stdin(account_file: Path):
     message = "some message"
+    private_key = None
+    private_key_file = str(account_file)
+    debug = "--no-debug"
+
     result = runner.invoke(
         app,
         [
             "account",
             "sign-bytes",
+            "--private-key-file", private_key_file,
+            debug
         ],
         input=message,
     )
@@ -95,4 +139,3 @@ def test_sign_raw_stdin():
     assert result.exit_code == 0
 
     assert "0x" in result.stdout
-
--- tests/unit/commands/test_file.py
+++ tests/unit/commands/test_file.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from tempfile import NamedTemporaryFile
 
 from typer.testing import CliRunner
@@ -7,13 +8,31 @@ from aleph_client.__main__ import app
 runner = CliRunner()
 
 
-def test_file_upload():
+def test_file_upload(account_file: Path):
+    path = None
+    channel = None
+    private_key = None
+    private_key_file = str(account_file)
+    ref = None
+    debug = "--no-debug"
+
     # Test upload a file to aleph network by creating a file and upload it to an aleph node
     with NamedTemporaryFile() as temp_file:
         temp_file.write(b"Hello World \n")
+
+        path = temp_file.name
+
         result = runner.invoke(
             app,
-            ["file", "upload", temp_file.name],
+            [
+                "file",
+                "upload",
+                path,
+                "--channel", channel,
+                "--private-key-file", private_key_file,
+                ref,
+                debug
+            ],
         )
 
         assert result.exit_code == 0
@@ -22,13 +41,25 @@ def test_file_upload():
 
 
 def test_file_download():
+    hash = "QmeomffUNfmQy76CQGy9NdmqEnnHU9soCexBnGU3ezPHVH"
+    use_ipfs = "--no-use-ipfs"
+    output_path = "."
+    file_name = None
+    file_extension = None
+    debug = "--no-debug"
+
     # Test download a file to aleph network
     result = runner.invoke(
         app,
         [
             "file",
             "download",
-            "QmeomffUNfmQy76CQGy9NdmqEnnHU9soCexBnGU3ezPHVH",
+            hash,
+            use_ipfs,
+            "--output-path", output_path,
+            "--file-name", file_name,
+            "--file-extension", file_extension,
+            debug
         ],  # 5 bytes file
     )
 
