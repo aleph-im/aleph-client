fb21fd32c4d39fbc4ef866846aa22d364b3bdd95
Álvaro E. F. Mota <alvaroefmota@gmail.com>
2024-02-07 17:56:48 -0300

    Updating unit tests from program command.

--- tests/unit/commands/test_program.py
+++ tests/unit/commands/test_program.py
@@ -5,9 +5,82 @@ from pathlib import Path
 from typer.testing import CliRunner
 
 from aleph_client.__main__ import app
+import pytest
 
 runner = CliRunner()
 
+
+@pytest.mark.skip(reason="Not implemented.")
+def test_program_unpersist(account_file: Path):
+    item_hash = "item_hash"
+    private_key = None
+    private_key_file = str(account_file)
+    debug = "--debug" # [--debug|--no-debug]
+
+    result = runner.invoke(
+        app, [
+            "program", "unpersist", item_hash,
+            "--private-key-file", private_key_file,
+            debug,
+        ]
+    )
+
+    assert result.exit_code == 0
+    assert result.stdout
+
+@pytest.mark.skip(reason="Not implemented.")
+def test_program_upload(account_file: Path):
+    path = "path"
+    entrypoint = "entrypoint"
+    channel = "channel"
+    memory = "memory"
+    vcpus = "vcpus"
+    timeout_seconds = "tomeout_seconds"
+    private_key = None
+    private_key_file = str(account_file)
+    print_messages = "--print-messages" # [--print-message|--no-print-message]
+    print_code_message = "--print-code-message" # [--print-code-message|--no-print-code-message]
+    print_program_message = "--print-program-message" # [--print-program-message|--no-print-program-message]
+    runtime = "runtime"
+    beta = "--beta" # [--beta|--no-beta]
+    debug = "--debug" # [--debug|--no-debug]
+    persistent = "--persistent" # [--persistent|--no-persistent]
+    persistent_volume = "persistent_volume"
+
+    result = runner.invoke(
+        app, [
+            "program", "update", path, entrypoint,
+
+
+            "--private-key-file", private_key_file,
+            print_message,
+            debug,
+        ]
+    )
+
+    assert result.exit_code == 0
+    assert result.stdout
+
+@pytest.mark.skip(reason="Not implemented.")
+def test_program_update(account_file: Path):
+    item_hash = "item_hash"
+    path = "path"
+    private_key = None
+    private_key_file = str(account_file)
+    print_message = "--print-message" # [--print-message|--no-print-message]
+    debug = "--debug" # [--debug|--no-debug]
+
+    result = runner.invoke(
+        app, [
+            "program", "update", item_hash, path,
+            "--private-key-file", private_key_file,
+            print_message,
+            debug,
+        ]
+    )
+
+    assert result.exit_code == 0
+    assert result.stdout
 
 def test_program_unpersist(account_file: Path):
     item_hash = "098f6bcd4621d373cade4e832627b4f6"
