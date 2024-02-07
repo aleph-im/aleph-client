c34a803416a76a3c0f1bdab857c8ce06717bec4c
Álvaro E. F. Mota <alvaroefmota@gmail.com>
2024-02-07 17:35:47 -0300

    Updating unit tests from instance command.

--- tests/unit/commands/test_instance.py
+++ tests/unit/commands/test_instance.py
@@ -6,6 +6,90 @@ import pytest
 runner = CliRunner()
 item_hash = None
 
+@pytest.mark.skip(reason="Not implemented.")
+def test_instance_create(account_file: Path):
+	channel = "channel"
+	memory = "memory"
+	vcpus = "vcpus"
+	timeout_seconds = "timeout_seconds"
+	private_key = None
+	private_key_file = str(account_file)
+	ssh_pubkey_file = "ssh_pubkey_file"
+	print_messages = "--print-messages" #[--print-messages|--no-print-messages]
+	rootfs = "rootfs"
+	rootfs_name = "rootfs_name"
+	rootfs_size = "rootfs_size"
+	debug = "--debug" # [--debug|--no-debug]
+	persistent_volume = "persistent_volume"
+	ephemeral_volume = "ephemeral_volume"
+	immutable_volume = "immutable_volume"
+
+	result = runner.invoke(
+		app, [
+			"instance", "create",
+			"--channel", channel,
+			"--memory", memory,
+			"--vcpus", vcpus,
+			"--timeout-seconds", timeout_seconds,
+			"--private-key-file", private_key_file,
+			"--ssh-pubkey-file", ssh_pubkey_file,
+			print_messages,
+			"--rootfs", rootfs,
+			"--rootfs-name", rootfs_name,
+			"--rootfs-size", rootfs_size,
+			debug,
+			"--persistent-volume", persistent_volume,
+			"--ephemeral-volume", ephemeral_volume,
+			"--imutable-volume", immutable_volume
+		]
+	)
+
+	assert result.exit_code == 0
+	assert result.stdout
+
+@pytest.mark.skip(reason="Not implemented.")
+def test_instance_delete(account_file: Path):
+	item_hash = "item_hash"
+	reason = "reason"
+	private_key = None
+	private_key_file = str(account_file)
+	print_messages = "--print-messages" #[--print-messages|--no-print-messages]
+	debug = "--debug" # [--debug|--no-debug]
+
+	result = runner.invoke(
+		app, [
+			"instance", "delete",
+			item_hash,
+			"--reason", reason,
+			"--private-key-file", private_key_file,
+			print_messages,
+			debug,
+		]
+	)
+
+	assert result.exit_code == 0
+	assert result.stdout
+
+@pytest.mark.skip(reason="Not implemented.")
+def test_instance_list(account_file: Path):
+	address = "address"
+	private_key = None
+	private_key_file = str(account_file)
+	json = "--json" # [--json|--no-json]
+	debug = "--debug" # [--debug|--no-debug]
+
+	result = runner.invoke(
+		app, [
+			"instance", "list",
+			"--address", address
+			"--private-key-file", private_key_file,
+			json,
+			debug,
+		]
+	)
+
+	assert result.exit_code == 0
+	assert result.stdout
 
 @pytest.mark.skip(reason="Not implemented.")
 def test_instance_create():
