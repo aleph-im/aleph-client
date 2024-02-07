b4d19a7d9d9d8c117c9e6bd270f3ba69cfd23aff
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-02-07 20:33:35 +0000

    Adding unit tests for the file command.

--- tests/unit/commands/test_file.py
+++ tests/unit/commands/test_file.py
@@ -66,3 +66,107 @@ def test_file_download():
     assert result.exit_code == 0
 
     assert result.stdout is not None
+
+
+def test_file_forget(account_file: Path):
+    item_hash = "QmeomffUNfmQy76CQGy9NdmqEnnHU9soCexBnGU3ezPHVH"
+    reason = "reason"
+    channel = ""
+    private_key = None
+    private_key_file = str(account_file)
+    debug = "--no-debug"
+
+    result = runner.invoke(
+        app,
+        [
+            "file",
+            "forget",
+            item_hash,
+            reason,
+            "--channel", channel,
+            "--private-key-file", private_key_file,
+            debug
+        ],
+    )
+
+    assert result.exit_code == 0
+
+    assert result.stdout is not None
+
+
+def test_file_forget(account_file: Path):
+    item_hash = "QmeomffUNfmQy76CQGy9NdmqEnnHU9soCexBnGU3ezPHVH"
+    reason = "reason"
+    channel = ""
+    private_key = None
+    private_key_file = str(account_file)
+    debug = "--no-debug"
+
+    result = runner.invoke(
+        app,
+        [
+            "file",
+            "forget",
+            item_hash,
+            reason,
+            "--channel", channel,
+            "--private-key-file", private_key_file,
+            debug
+        ],
+    )
+
+    assert result.exit_code == 0
+
+    assert result.stdout is not None
+
+
+def test_file_list(account_file: Path):
+    address = None
+    private_key = None
+    private_key_file = str(account_file)
+    pagination = 100
+    page = 1
+    sort_order = -1
+    json = "--no-json"
+
+    result = runner.invoke(
+        app,
+        [
+            "file",
+            "list",
+            "--address", address,
+            "--private-key-file", private_key_file,
+            "--pagination", pagination,
+            "--page", page,
+            "--sort-order", sort_order,
+            "--json", json
+        ],  # 5 bytes file
+    )
+
+    assert result.exit_code == 0
+
+    assert result.stdout is not None
+
+
+def test_file_pin(account_file: Path):
+    item_hash = "QmeomffUNfmQy76CQGy9NdmqEnnHU9soCexBnGU3ezPHVH"
+    channel = None
+    private_key = None
+    private_key_file = str(account_file)
+    ref = None
+    debug = "--no-debug"
+
+    result = runner.invoke(
+        app,
+        [
+            "file",
+            "pin",
+            item_hash,
+            "--channel", channel,
+            "--private-key-file", private_key_file,
+            "--ref", ref,
+            debug
+        ],
+    )
+
+    assert result.exit_code == 0
