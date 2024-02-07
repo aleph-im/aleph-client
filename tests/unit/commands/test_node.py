25c98b9be6ad7fbc2dccd5960cde607dff2b46e8
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-02-07 20:39:39 +0000

    Adding unit tests for the node command.

--- tests/unit/commands/test_node.py
+++ tests/unit/commands/test_node.py
@@ -1,20 +1,52 @@
 import re
 
-from pathlib import Path
-
-import pytest
 from typer.testing import CliRunner
 
 from aleph_client.__main__ import app
 
 runner = CliRunner()
 
+
 def test_node_compute():
+	json = "--no-json"
+	active = "--no-active"
+	address = None
+	debug = "--no-debug"
+
 	result = runner.invoke(
-		app, ["node", "compute"]
+		app, [
+			"node",
+			"compute",
+			json,
+			active,
+			"--address", address,
+			debug
+		]
 	)
+
 	assert result.exit_code == 0
+
 	pattern = r".*Compute Node Information.*"
 	assert re.match(pattern, result.stdout)
 	# pattern = r".*?([0-9]+\.[0-9]+%|100\.00%|0\.00%).*?([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}).*?([0-9]+\.[0-9]+%|100\.00%).*?([a-z]+).*?"
 	# assert len(re.findall(pattern, result.stdout, re.MULTILINE)) > 0
+
+
+def test_node_core():
+	json = "--no-json"
+	active = "--no-active"
+	address = None
+	debug = "--no-debug"
+
+	result = runner.invoke(
+		app, [
+			"node",
+			"core",
+			json,
+			active,
+			"--address", address,
+			debug
+		]
+	)
+
+	assert result.exit_code == 0
