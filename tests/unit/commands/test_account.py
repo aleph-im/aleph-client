43f89fc71172ba3aa4e1e1f9e4651814d4d59a54
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-02-26 22:26:32 +0000

    Fixing and improving account unit tests.

--- tests/unit/commands/test_account.py
+++ tests/unit/commands/test_account.py
@@ -1,6 +1,5 @@
+import re
 from pathlib import Path
-
-import pytest
 from typer.testing import CliRunner
 
 from aleph_client.__main__ import app
@@ -9,7 +8,7 @@ runner = CliRunner()
 
 
 def test_account_address(account_file: Path):
-    private_key = None
+    # private_key = None
     private_key_file = str(account_file)
 
     result = runner.invoke(
@@ -22,14 +21,16 @@ def test_account_address(account_file: Path):
 
     assert result.exit_code == 0
 
-    assert result.stdout.startswith("0x")
+    pattern = r"0x.*"
+    assert re.match(pattern, result.stdout)
 
     assert len(result.stdout.strip()) == 42
 
 
+# TODO Verify if the output message ""Failed to retrieve balance..." is ok!!!
 def test_account_balance(account_file: Path):
     address = None
-    private_key = None
+    # private_key = None
     private_key_file = str(account_file)
 
     result = runner.invoke(
@@ -41,11 +42,14 @@ def test_account_balance(account_file: Path):
         ]
     )
 
+    pattern = r"Failed to retrieve balance for address 0x.*\. Status code: 404"
+    assert re.match(pattern, result.stdout)
+
     assert result.exit_code == 0
 
 
 def test_account_create(account_file: Path):
-    private_key = None
+    # private_key = None
     private_key_file = str(account_file)
     replace = "--replace"
     debug = "--no-debug"
@@ -66,11 +70,14 @@ def test_account_create(account_file: Path):
 
     new_key = account_file.read_bytes()
 
+    pattern = r"Private key stored in .*"
+    assert re.match(pattern, result.stdout)
+
     assert new_key != old_key
 
 
 def test_account_export_private_key(account_file: Path):
-    private_key = None
+    # private_key = None
     private_key_file = str(account_file)
 
     result = runner.invoke(
@@ -84,6 +91,9 @@ def test_account_export_private_key(account_file: Path):
 
     assert result.stdout.startswith("0x")
 
+    pattern = r"0x.*"
+    assert re.match(pattern, result.stdout)
+
     assert len(result.stdout.strip()) == 66
 
 
@@ -93,12 +103,13 @@ def test_account_path():
         "path"
     ])
 
-    assert result.stdout.startswith("/")
+    pattern = r".*.aleph-im/private-keys/ethereum\.key"
+    assert re.match(pattern, result.stdout)
 
 
 def test_sign_bytes_raw(account_file: Path):
     message = "some message"
-    private_key = ""
+    # private_key = None
     private_key_file = str(account_file)
     debug = "--no-debug"
 
@@ -115,12 +126,13 @@ def test_sign_bytes_raw(account_file: Path):
 
     assert result.exit_code == 0
 
-    assert "0x" in result.stdout
+    pattern = r"0x.*"
+    assert re.match(pattern, result.stdout)
 
 
 def test_sign_bytes_raw_stdin(account_file: Path):
     message = "some message"
-    private_key = None
+    # private_key = None
     private_key_file = str(account_file)
     debug = "--no-debug"
 
@@ -137,4 +149,5 @@ def test_sign_bytes_raw_stdin(account_file: Path):
 
     assert result.exit_code == 0
 
-    assert "0x" in result.stdout
+    pattern = r"0x.*"
+    assert re.match(pattern, result.stdout)
