a1fb18eb6ee19cf395cc1cb8fa122e7dc224af4b
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-03-11 20:58:13 +0000

    Improving and fixing test_node.py unit tests.

--- tests/unit/commands/test_node.py
+++ tests/unit/commands/test_node.py
@@ -1,3 +1,5 @@
+import textwrap
+
 import re
 
 from typer.testing import CliRunner
@@ -8,45 +10,67 @@ runner = CliRunner()
 
 
 def test_node_compute():
-	json = "--no-json"
-	active = "--no-active"
-	address = None
-	debug = "--no-debug"
+    json = "--no-json"
+    active = "--no-active"
+    address = None
+    debug = "--no-debug"
 
-	result = runner.invoke(
-		app, [
-			"node",
-			"compute",
-			json,
-			active,
-			"--address", address,
-			debug
-		]
-	)
+    result = runner.invoke(
+        app, [
+            "node",
+            "compute",
+            json,
+            active,
+            "--address", address,
+            debug
+        ]
+    )
 
-	assert result.exit_code == 0
+    assert result.exit_code == 0
 
-	pattern = r".*Compute Node Information.*"
-	assert re.match(pattern, result.stdout)
-	# pattern = r".*?([0-9]+\.[0-9]+%|100\.00%|0\.00%).*?([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}).*?([0-9]+\.[0-9]+%|100\.00%).*?([a-z]+).*?"
-	# assert len(re.findall(pattern, result.stdout, re.MULTILINE)) > 0
+    pattern = textwrap.dedent(
+        '''\
+        .*Compute Node Information.*
+        .*
+        .* Score ┃ Name                        ┃    Creation Time    ┃ Decentralization ┃  Status.*
+        .*
+        │ [0-9]+\.[0-9]+% │ .* │ [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} │ .*[0-9]+\.[0-9]+% │ .* │
+        [\s\S]*
+        '''
+    )
+
+    assert re.fullmatch(pattern, result.stdout)
 
 
+# TODO Stopped here!!!
 def test_node_core():
-	json = "--no-json"
-	active = "--no-active"
-	address = None
-	debug = "--no-debug"
+    json = "--no-json"
+    active = "--no-active"
+    address = None
+    debug = "--no-debug"
 
-	result = runner.invoke(
-		app, [
-			"node",
-			"core",
-			json,
-			active,
-			"--address", address,
-			debug
-		]
-	)
+    result = runner.invoke(
+        app, [
+            "node",
+            "core",
+            json,
+            active,
+            "--address", address,
+            debug
+        ]
+    )
 
-	assert result.exit_code == 0
+    assert result.exit_code == 0
+
+    pattern = textwrap.dedent(
+        '''\
+        .*Core Channel Node Information.*
+        .*
+        .* Score ┃ Name                           ┃ Staked    ┃ Linked ┃    Creation Time    ┃  Status
+        .*
+        │ [0-9]+\.[0-9]+% │ .* │ .* │ [0-9]+ │ [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} │ .*  │
+        [\s\S]*
+        '''
+    )
+
+    assert re.fullmatch(pattern, result.stdout)
