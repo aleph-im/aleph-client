a42783598fa7cf41c155024e1a79c9eb4751c533
mhh <mike.hukiewitz@robotter.ai>
2024-04-29 09:48:12 +0200

    WIP instance tests

--- tests/unit/commands/test_instance.py
+++ tests/unit/commands/test_instance.py
@@ -1,25 +1,21 @@
 from typer.testing import CliRunner
 from pathlib import Path
 from aleph_client.__main__ import app
-import re
-import pytest
 
 runner = CliRunner()
 item_hash = None
 
 
-# TODO Stopped here!!!
+# TODO: Add non-interactive flag
 def test_instance_create(account_file: Path, ssh_keys_files: dict[str, Path]):
 	channel = "channel"
 	memory = 256
 	vcpus = 1
 	timeout_seconds = 30.0
-	# private_key = None
 	private_key_file = str(account_file)
 	ssh_pubkey_file = str(ssh_keys_files["public_key"])
 	print_messages = "--no-print-messages"
 	rootfs = "Ubuntu 22"
-	rootfs_name = "main-rootfs"
 	rootfs_size = 2000
 	debug = "--no-debug"
 	persistent_volume = None
@@ -37,7 +33,6 @@ def test_instance_create(account_file: Path, ssh_keys_files: dict[str, Path]):
 			"--ssh-pubkey-file", ssh_pubkey_file,
 			print_messages,
 			"--rootfs", rootfs,
-			"--rootfs-name", rootfs_name,
 			"--rootfs-size", rootfs_size,
 			debug,
 			"--persistent-volume", persistent_volume,
@@ -50,9 +45,6 @@ def test_instance_create(account_file: Path, ssh_keys_files: dict[str, Path]):
 	print(result.exit_code)
 	print("result.stdout:")
 	print(result.stdout)
-	# from tests.unit.test_utils import dump
-	# print("dump:")
-	# print(dump(result))
 
 	assert result.exit_code == 0
 	# assert result.stdout
@@ -61,7 +53,6 @@ def test_instance_create(account_file: Path, ssh_keys_files: dict[str, Path]):
 def test_instance_delete(account_file: Path):
 	item_hash = "item_hash"
 	reason = "reason"
-	private_key = None
 	private_key_file = str(account_file)
 	print_messages = "--print-messages" #[--print-messages|--no-print-messages]
 	debug = "--debug" # [--debug|--no-debug]
@@ -83,7 +74,6 @@ def test_instance_delete(account_file: Path):
 
 def test_instance_list(account_file: Path):
 	address = "address"
-	private_key = None
 	private_key_file = str(account_file)
 	json = "--json" # [--json|--no-json]
 	debug = "--debug" # [--debug|--no-debug]
