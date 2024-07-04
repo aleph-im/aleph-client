7a49a81fd5cc1ed184ad58ccf1b8845f39ceadf7
Andres D. Molins <amolinsdiaz@yahoo.es>
2024-07-04 21:51:10 +0200

    Problem: If a user wants to create a Qemu instance, he can but the VM never boots because have an incorrect rootfs.
    Solution: Add the proper rootfs hashes for Qemu hypervisor.

--- src/aleph_client/commands/instance/__init__.py
+++ src/aleph_client/commands/instance/__init__.py
@@ -110,10 +110,16 @@ async def create(
 
     account: AccountFromPrivateKey = _load_account(private_key, private_key_file)
 
-    os_map = {
+    os_firecracker_map = {
         settings.UBUNTU_22_ROOTFS_ID: "Ubuntu 22",
         settings.DEBIAN_12_ROOTFS_ID: "Debian 12",
         settings.DEBIAN_11_ROOTFS_ID: "Debian 11",
+    }
+
+    os_qemu_map = {
+        settings.UBUNTU_22_QEMU_ROOTFS_ID: "Ubuntu 22",
+        settings.DEBIAN_12_QEMU_ROOTFS_ID: "Debian 12",
+        settings.DEBIAN_11_QEMU_ROOTFS_ID: "Debian 11",
     }
 
     hv_map = {
@@ -132,10 +138,19 @@ async def create(
             lambda x: x in valid_address,
         )
 
+    hypervisor = HypervisorType[
+        Prompt.ask(
+            "Which hypervisor you want to use?",
+            default=hypervisor.name,
+            choices=[*hv_map.values()],
+        )
+    ]
+
+    choices = os_qemu_map if hypervisor == HypervisorType.qemu else os_firecracker_map
     rootfs = Prompt.ask(
         "Do you want to use a custom rootfs or one of the following prebuilt ones?",
         default=rootfs,
-        choices=[*os_map.values(), "custom"],
+        choices=[*choices.values(), "custom"],
     )
 
     if rootfs == "custom":
@@ -144,7 +159,7 @@ async def create(
             lambda x: len(x) == 64,
         )
     else:
-        rootfs = next(k for k, v in os_map.items() if v == rootfs)
+        rootfs = next(k for k, v in choices.items() if v == rootfs)
 
     async with AlephHttpClient(api_server=sdk_settings.API_HOST) as client:
         rootfs_message: StoreMessage = await client.get_message(item_hash=rootfs, message_type=StoreMessage)
@@ -159,14 +174,6 @@ async def create(
     memory = validated_int_prompt("Maximum memory allocation on vm in MiB", memory, min_value=2000, max_value=8000)
 
     rootfs_size = validated_int_prompt("Disk size in MiB", rootfs_size, min_value=20000, max_value=100000)
-
-    hypervisor = HypervisorType[
-        Prompt.ask(
-            "Which hypervisor you want to use?",
-            default=hypervisor.name,
-            choices=[*hv_map.values()],
-        )
-    ]
 
     volumes = get_or_prompt_volumes(
         persistent_volume=persistent_volume,
