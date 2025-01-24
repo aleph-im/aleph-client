e038bdea0ef5eda838fe162567ef06b0a8e9985b
philogicae <38438271+philogicae@users.noreply.github.com>
2025-01-24 17:13:08 +0200

    wip 1

--- src/aleph_client/commands/website.py
+++ src/aleph_client/commands/website.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
-import json
 import logging
 import re
 from base64 import b16decode, b32encode
 from collections.abc import Mapping
+from json import dumps
 from pathlib import Path
-from typing import List, Optional, cast
+from typing import Dict, List, Optional, cast
 from zipfile import BadZipFile
 
 import aiohttp
@@ -24,7 +24,7 @@ from aleph_message.models.execution.program import ProgramContent
 from aleph_message.models.item_hash import ItemHash
 from aleph_message.status import MessageStatus
 from click import echo
-from rich import box
+from rich import box, print
 from rich.console import Console
 from rich.panel import Panel
 from rich.prompt import Prompt
@@ -63,6 +63,8 @@ async def create(
     setup_logging(debug)
 
     path = path.absolute()
+
+    return None
 
 
 @app.command()
@@ -115,6 +117,111 @@ async def list_websites(
         account = _load_account(private_key, private_key_file)
         address = account.get_address()
 
+    async with AlephHttpClient(api_server=settings.API_HOST) as client:
+        resp = None
+        try:
+            resp = await client.fetch_aggregates(address=address, keys=["websites", "domains"])
+        except aiohttp.ClientConnectorError as e:
+            echo(f"Unable to connect to API server (CCN)\nError: {e}")
+        except aiohttp.ClientResponseError:
+            pass
+        if not resp:
+            typer.echo(f"Address: {address}\n\nNo website found\n")
+            raise typer.Exit(code=1)
+
+        websites, linked_domains = {}, {}
+        if "domains" in resp:
+            found_domains: Dict[str, List[str]] = {}
+            for domain, content in resp["domains"].items():
+                if content and content["type"] == "ipfs":
+                    version = content["message_id"]
+                    found_domains[version] = found_domains.get(version, [])
+                    found_domains[version].append(domain)
+            linked_domains.update(found_domains)
+
+        if "websites" in resp:
+            found_websites: Dict[str, Dict] = {}
+            for sitename, content in resp["websites"].items():
+                if content:
+                    found_websites[sitename] = content
+                    current_version = content["volume_id"]
+                    if current_version in linked_domains:
+                        content["linked_domains"] = content.get("linked_domains", {})
+                        content["linked_domains"]["current"] = linked_domains[current_version]
+                    if "history" in content:
+                        for version, volume_id in content["history"].items():
+                            if volume_id in linked_domains:
+                                domains = content["linked_domains"] = content.get("linked_domains", {})
+                                legacy = domains["legacy"] = domains.get("legacy", {})
+                                legacy[version] = legacy.get(version, [])
+                                legacy[version].extend(linked_domains[volume_id])
+            websites.update(found_websites)
+
+        if not websites:
+            typer.echo(f"Address: {address}\n\nNo website found\n")
+            raise typer.Exit(code=1)
+        if json:
+            print(dumps(websites, indent=4))
+        else:
+            table = Table(box=box.ROUNDED, style="blue_violet")
+            table.add_column(f"Websites [{len(websites)}]", style="blue", overflow="fold")
+            table.add_column("Specifications", style="blue")
+            table.add_column("Linked Domains", style="blue", overflow="fold")
+
+            for sitename, details in websites.items():
+                name = Text(sitename, style="magenta3")
+                current_volume = details["volume_id"]
+                msg_link = f"https://explorer.aleph.im/address/ETH/{address}/message/STORE/{current_volume}"
+                item_hash_link = Text.from_markup(f"[link={msg_link}]{current_volume}[/link]", style="bright_cyan")
+                created_at = Text.assemble(
+                    "Created at: ",
+                    Text(
+                        str(str_to_datetime(str(details["created_at"]))).split(".", maxsplit=1)[0],
+                        style="orchid",
+                    ),
+                )
+                updated_at = Text.assemble(
+                    "Updated at: ",
+                    Text(
+                        str(str_to_datetime(str(details["updated_at"]))).split(".", maxsplit=1)[0],
+                        style="orchid",
+                    ),
+                )
+                website = Text.assemble(
+                    "Item Hash ↓\t     Name: ", name, "\n", item_hash_link, "\n", created_at, "  ", updated_at
+                )
+                specs = [
+                    (
+                        f"Framework: [magenta3]{details["metadata"]['framework']}[/magenta3]"
+                        if "framework" in details["metadata"]
+                        else ""
+                    ),
+                    f"Current Version: [magenta3]{details['version']}[/magenta3]",
+                    f"History Size: [magenta3]{len(details.get('history', {}))}[/magenta3]",
+                ]
+                specifications = Text.from_markup("\n".join(specs))
+                url = ""  # TODO: Calc v1 CID for default url
+                domains = Text.from_markup(f"[bright_yellow][link={url}]{url}[/link][/bright_yellow]")
+                table.add_row(website, specifications, domains)
+                table.add_section()
+
+            console = Console()
+            console.print(table)
+            infos = [
+                Text.from_markup(
+                    f"[bold]Address:[/bold] [bright_cyan]{address}[/bright_cyan]\n\nTo check all available commands, use:\n"
+                ),
+                Text.from_markup(
+                    "↳ aleph website --help",
+                    style="italic",
+                ),
+            ]
+            console.print(
+                Panel(
+                    Text.assemble(*infos), title="Infos", border_style="bright_cyan", expand=False, title_align="left"
+                )
+            )
+
 
 @app.command()
 async def history(
@@ -127,6 +234,6 @@ async def history(
     verbose: bool = True,
     debug: bool = False,
 ):
-    """List or prune previous versions of a website on aleph.im"""
+    """List, prune, or revert to previous versions of a website"""
 
     setup_logging(debug)
